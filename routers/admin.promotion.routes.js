dc21d4626daa7ed0a2c175871f24f695c33531d3
shafayat783593 <sshapa17@gmail.com>
2026-04-20 22:17:26 +0600

    create admin promotion

---
+++ routers/admin.promotion.routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+    createPromotion,
+    getAllPromotions,
+    getPromotionById,
+    updatePromotion,
+    deletePromotion
+} from "../controllers/promotion.controller.js";
+
+const router = express.Router();
+
+router.post('/add', createPromotion);
+router.get('/', getAllPromotions);
+router.get('/:id', getPromotionById);
+router.put('/:id', updatePromotion);
+router.delete('/:id', deletePromotion);
+
+export default router;
